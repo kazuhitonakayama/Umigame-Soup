8b2101b59ae91163bb0c1efb9a98c2577ff204d3
kazuhitonakayama <firstjin19990101@gmail.com>
2020-05-13 15:51:36 +0900

    Set Login_UserId_To_Each_Post

---
+++ db/migrate/20200513064200_add_references_to_posts.rb
@@ -0,0 +1,5 @@
+class AddReferencesToPosts < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :posts, :user, null: false, default: 1 ,foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_053458) do
+ActiveRecord::Schema.define(version: 2020_05_13_064200) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "post_id", null: false
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2020_05_13_053458) do
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", default: 1, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_05_13_053458) do
 
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
