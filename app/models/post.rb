c83c2ddb9d9e88309c5d0e586f5dee33d4a17966
kazuhitonakayama <firstjin19990101@gmail.com>
2020-05-22 10:42:08 +0900

    Debuging

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,7 @@ class Post < ApplicationRecord
     has_many :answers, dependent: :destroy
     belongs_to :user
     has_many :liked_users, through: :likes, source: :user # liked_usersによって投稿が誰にいいねされているのかを簡単に取得できるようになります。
+    def already_liked?(post)
+        self.likes.exists?(post_id: post.id)
+    end
 end
