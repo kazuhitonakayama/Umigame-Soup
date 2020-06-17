0a7dc01e8d17b6f7cfe9782ddef94322264a608f
kazuhitonakayama <firstjin19990101@gmail.com>
2020-06-17 23:51:38 +0900

    Header responsiblize finished

---
+++ app/javascript/packs/layouts/application.js
@@ -0,0 +1,9 @@
+(function(){
+    
+    const $header = document.querySelector('.header');
+    //addEventListener：イベント処理（click）
+    $header.addEventListener('click', function(){
+      //ON時はis-activeクラス付与、OFF時はis-activeクラス削除
+      $header.classList.toggle('is-active')
+    });
+})();
